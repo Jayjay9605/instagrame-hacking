9061f1917589716a075ea9eb2121a925cab43375
Jayjay9605 <133637505+Jayjay9605@users.noreply.github.com>
2023-05-15 03:47:34 -0400

    Update instagrame-hacking.py

--- instagrame-hacking.py
+++ instagrame-hacking.py
@@ -32,7 +32,7 @@ class InstaBrute(object):
     def __init__(self):
 
         try:
-            user = input('username : ')
+            user = input('samira_sofia24 : ')
             Combo = input('passList : ')
             print('\n----------------------------')
 
